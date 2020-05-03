8ee22582ef875a1ffaf9ef28915447d20fd29976
fsciuti <fsciuti@gmail.com>
2020-05-03 23:42:03 +0200

    build: update webpack config with eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        contentBase: './dist'
-    }
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {},
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
 };
